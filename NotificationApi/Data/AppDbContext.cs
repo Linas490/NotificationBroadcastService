91baa6a65e575a2e309bc0adb299aab9d900a937
Linas490 <113028160+Linas490@users.noreply.github.com>
2025-07-22 11:34:01 +0300

    Added AppDbCintext.

---
+++ NotificationApi/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationApi.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace NotificationApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- NotificationApi/Program.cs
+++ NotificationApi/Program.cs
@@ -1,3 +1,5 @@
+using NotificationApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
