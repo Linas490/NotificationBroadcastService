04bd746cd30c459f23b1c72714c6abdcf54443d6
Linas490 <113028160+Linas490@users.noreply.github.com>
2025-07-22 11:48:47 +0300

    Notification repository implemented - used to communicate with database to post notifications are retreive last n notifications.

---
+++ NotificationApi/Repositories/Interfaces/INotificationRepository.cs
@@ -0,0 +1,10 @@
+﻿using NotificationApi.Models;
+
+namespace NotificationApi.Repositories.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task<bool> AddNotificationAsync(Notification notification);
+        Task<IEnumerable<Notification>> GetLastNotificationsAsync(int limit);
+    }
+}
---
+++ NotificationApi/Repositories/Repositories/NotificationRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationApi.Data;
+using NotificationApi.Models;
+using NotificationApi.Repositories.Interfaces;
+using System.Collections.Generic;
+
+namespace NotificationApi.Repositories.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly AppDbContext _context;
+
+        public NotificationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddNotificationAsync(Notification notification)
+        {
+            await _context.Notifications.AddAsync(notification);
+            var changes = await _context.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<IEnumerable<Notification>> GetLastNotificationsAsync(int limit)
+        {
+            return await _context.Notifications
+                .OrderByDescending(n => n.Timestamp)
+                .Take(limit)
+                .ToListAsync();
+        }
+    }
+}
