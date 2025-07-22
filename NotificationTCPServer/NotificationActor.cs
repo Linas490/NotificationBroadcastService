0d41bac01418f6baf8bdc94f263a4f03cb54b2e4
Linas490 <113028160+Linas490@users.noreply.github.com>
2025-07-22 14:18:16 +0300

    Updated NotificationTCPServer to .NET 8

---
+++ NotificationTCPServer/NotificationActor.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationTCPServer
+{
+    public class NotificationActor : ReceiveActor
+    {
+        private readonly HashSet<TcpClient> _clients = new HashSet<TcpClient>();
+        public NotificationActor()
+        {
+            Receive<AddClient>(msg => 
+            {
+                _clients.Add(msg.Client);
+            });
+
+            Receive<RemoveClient>(msg =>
+            {
+                _clients.Remove(msg.Client);
+            });
+
+            Receive<Notification>(msg => {
+                var data = Encoding.UTF8.GetBytes(msg.Message + "\n");
+
+                foreach (var client in _clients)
+                {
+                    try
+                    {
+                        if (client.Connected)
+                        {
+                            client.GetStream().Write(data, 0, data.Length);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _clients.Remove(client);
+                    }
+                }
+            });
+        }
+    }
+    public record AddClient(TcpClient Client);
+    public record RemoveClient(TcpClient Client);
+    public record Notification(string Message);
+}
--- NotificationTCPServer/Program.cs
+++ NotificationTCPServer/Program.cs
@@ -1,19 +1,11 @@
-﻿using System;
+﻿using NotificationTCPServer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NotificationTCPServer
-{
-    internal class Program
-    {
-        static async Task Main(string[] args)
-        {
-            int port = 5000;
-            var server = new TCPServer(port);
+int port = 5000;
+var server = new TCPServer(port);
 
-            await server.ListenAsync();
-        }
-    }
-}
+await server.ListenAsync();
